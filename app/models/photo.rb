1f6cb98f9ad26af16b8d2a9d92441a247cb27014
Sherlynn Tan <1sherlynn@gmail.com>
2017-09-13 17:10:46 +0800

    add photo controller, CRUD actions, views and associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,5 @@
 class Photo < ApplicationRecord
+	belongs_to :album
+	has_many :comments
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "photos#index"
+
+  resources :albums do 
+  	resources :photos do 
+  		resources :comments 
+  	end
+  end
+
+
 end
